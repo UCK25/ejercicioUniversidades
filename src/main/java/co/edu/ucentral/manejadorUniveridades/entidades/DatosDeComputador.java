e4b19b3b0b84a38c1c8cf0eb1181e82a321ac9ca
kenka <kenka@micorreo.com>
2025-08-15 10:44:24 -0500

    feature: prueba

--- src/main/java/co/edu/ucentral/manejadorUniveridades/entidades/DatosDeComputador.java
+++ src/main/java/co/edu/ucentral/manejadorUniveridades/entidades/DatosDeComputador.java
@@ -1,4 +1,25 @@
 package co.edu.ucentral.manejadorUniveridades.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "computadoras")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class DatosDeComputador {
+    @Id
+    private String cpu;
+    private String marca;
+    private String modelo;
+    private String capacidadAlmacenamiento;
+    private String memoriaRam;
 }
